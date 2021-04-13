1e56fb69cac1fb7865a81a95babc511dc1732b19
Lucas Silva <lucasmedja1@gmail.com>
2021-04-13 15:32:47 -0300

    feat/posts

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,22 @@
+import { useRouter } from 'next/dist/client/router';
+import Link from 'next/link';
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
 
 export function Header() {
+  const { asPath } = useRouter();
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <Link href="/">
+            <a className={asPath === '/' ? styles.active : ''}>Home</a>
+          </Link>
+          <Link href="/posts">
+            <a className={asPath === '/posts' ? styles.active : ''}>Posts</a>
+          </Link>
         </nav>
 
         <SignInButton />
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,6 +4,7 @@ import Providers from 'next-auth/providers';
 import { query as q } from 'faunadb';
 
 import { fauna } from '../../../services/fauna';
+import { session } from 'next-auth/client';
 
 export default NextAuth({
   
@@ -14,21 +15,39 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
-  jwt: {
-    signingKey: process.env.SIGNING_KEY,
-  },
   callbacks: {
     async signIn(user, account, profile) {
       const { email } = user
 
-      await fauna.query(
-        q.Create(
-          q.Collection('users'),
-          { data: { email } }
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
         )
-      )
 
-      return true
+        return true
+      } catch {
+        return false
+      }
+     
     },
   }
 })
